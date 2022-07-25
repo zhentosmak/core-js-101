beac83285e029f2c5b38f49f3cbfa4fdf08951f7
zhentosmak <zhentosmak@mail.ru>
2022-07-25 18:57:40 +0300

    fix: bug fix

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -51,9 +51,8 @@ function getStringLength(value) {
  *   'John','Doe'      => 'Hello, John Doe!'
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
-function getStringFromTemplate(/* firstName, lastName */) {
-  throw new Error('Not implemented');
-}
+ function getStringFromTemplate(firstName, lastName) {
+  return `Hello, ${firstName} ${lastName}!`;
 
 /**
  * Extracts a name from template string 'Hello, First_Name Last_Name!'.
