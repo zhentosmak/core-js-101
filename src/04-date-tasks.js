18d94a73ef2bc2d339e969b7e9925456b6fcdc0c
zhentosmak <zhentosmak@mail.ru>
2022-07-25 23:46:54 +0300

    feat: add objects task solutions

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
